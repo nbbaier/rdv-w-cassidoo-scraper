08cdce61edf4ac5afdcf9d67c7decbc41f310d1f
Nico Baier <nico.baier@gmail.com>
2025-11-18 18:15:30 -0600

    add all problems

--- src/pages/search.json.ts
+++ src/pages/search.json.ts
@@ -1,19 +1,20 @@
-import { getCollection } from 'astro:content';
+import { getCollection } from "astro:content";
 
 export async function GET() {
-	const questions = await getCollection('questions');
-	
+	const questions = await getCollection("questions");
+
 	const searchIndex = questions.map((question) => {
 		return {
 			slug: question.slug,
-			date: question.data.date.toISOString().split('T')[0],
+			date: question.data.date.toISOString().split("T")[0],
 			number: question.data.number,
 			// Create a simple excerpt by removing markdown syntax and taking the first 200 chars
-			excerpt: question.body
-				.replace(/[#*`\[\]()]/g, '') // Remove basic markdown chars
-				.replace(/\n+/g, ' ')        // Replace newlines with spaces
-				.trim()
-				.substring(0, 200) + '...'
+			excerpt:
+				question.body
+					.replace(/[#*`[\]()]/g, "") // Remove basic markdown chars
+					.replace(/\n+/g, " ") // Replace newlines with spaces
+					.trim()
+					.substring(0, 200) + "...",
 		};
 	});
 
@@ -22,7 +23,7 @@ export async function GET() {
 
 	return new Response(JSON.stringify(searchIndex), {
 		headers: {
-			'Content-Type': 'application/json',
+			"Content-Type": "application/json",
 		},
 	});
 }
